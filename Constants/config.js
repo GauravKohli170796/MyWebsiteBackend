b0416d062599694a1695e603eaec0942b925409e
Gaurav Kohli <gaurav.kohli@pinelabs.com>
2021-09-03 01:36:26 +0530

    mongo db url changes

--- Constants/config.js
+++ Constants/config.js
@@ -1,6 +1,6 @@
 module.exports =Object.freeze({
     Port: 4000, 
     UI_SERVER_URL:"http://localhost:3000",
-    MongoUrl: "mongodb://nodeazuremongodb:g7RO2nzTF5A5tRo4z6WJl9tP8Z4RB85GH8JtzBGS3qWZsfrs1sjMocp8P22Z21WWqtYQKAZXpvVadZY17yyXfg==@nodeazuremongodb.mongo.cosmos.azure.com:10255/MyWebsite?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@nodeazuremongodb@",                                         
+    MongoUrl: "mongodb+srv://Pallavi12345678:Pallavi12345678@cluster0.1zzns.mongodb.net",                                         
     MongoDB: "GauravWebsite",
 });
