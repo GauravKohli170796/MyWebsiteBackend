a7a1983327b2db3b623d6388650547d2722c9faf
Gaurav Kohli <gaurav.kohli@pinelabs.com>
2021-09-05 15:28:29 +0530

    adeed love calculator

--- Constants/config.js
+++ Constants/config.js
@@ -1,6 +1,6 @@
 module.exports =Object.freeze({
     Port: 4000, 
-    UI_SERVER_URL:/*"https://gaureesha-com.herokuapp.com",//*/"http://localhost:3000",
+    UI_SERVER_URL:"https://gaureesha-com.herokuapp.com",//"http://localhost:3000",
     MongoUrl: "mongodb+srv://Pallavi12345678:Pallavi12345678@cluster0.1zzns.mongodb.net",                                         
     MongoDB: "GauravWebsite",
 });
